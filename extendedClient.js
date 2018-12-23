fef1ab6fc5b1a5c8ca0dcf144672b17f49f8c166
sml32 <sml32minecraft@gmail.com>
2018-12-22 19:35:53 -0800

    Update extendedClient.js

--- extendedClient.js
+++ extendedClient.js
@@ -31,7 +31,3 @@ class ExtendedClient extends Client {
 }
 
 module.exports = ExtendedClient;
-
-const readConfig = () => {
-    return process.env.CONFIG_ARGBOT ? JSON.parse(process.env.CONFIG_ARGBOT) : require('./config.json');
-};
